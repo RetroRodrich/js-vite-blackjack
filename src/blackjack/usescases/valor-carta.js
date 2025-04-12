ff0876f11ef28b2010a9bc781dd485783dd48d1a
Rodrigo Quiroz <hrqa_20@outlook.com>
2025-04-12 02:20:47 -0500

    Cards Value Update

--- src/blackjack/usescases/valor-carta.js
+++ src/blackjack/usescases/valor-carta.js
@@ -8,6 +8,8 @@ export const valorCarta = (carta) => {
 
     const valor = carta.substring(0, carta.length - 1);
     return (isNaN(valor)) ?
-        (valor === 'A') ? 11 : 10
+        (valor === 'A') ? 14 :
+        (valor === 'K') ? 13 :
+        (valor === 'Q') ? 12 : 11
         : valor * 1;
 }
